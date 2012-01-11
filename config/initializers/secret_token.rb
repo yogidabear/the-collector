ff7efc9fbd7a6c85c99175f7f2e020282f8e1883
yogidabear <josh@compfactory.com>
2012-01-11 14:27:54 -0600

    Initial commit: Clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheCollector::Application.config.secret_token = '7cf463a56ce462bfa8c56ce1bb46158238b2da9571ae82f5e9e3af7eb522cf00a531e84f46df07425eba9d60b5de18cb484cc1108c4659c0d3fe23f27627b7b6'
